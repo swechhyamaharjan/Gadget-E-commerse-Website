e4faf71fbff3a208441be685ef3a46e969b736c8
Swechhya Maharjan <swekchya.mjn@gmail.com>
2026-03-04 14:10:52 +0545

    Added env variable in frontend and cors

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import uploadRouter from "./routers/uploadRouter.js"
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://gadget-ecommerce-backend.vercel.app"],
   credentials: true,
 }));
 
